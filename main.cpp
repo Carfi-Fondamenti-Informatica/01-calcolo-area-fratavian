dae3b568a82f76412a999ca7b2b0b34fc367ed0c
fratavian <117820691+fratavian@users.noreply.github.com>
2022-11-11 23:57:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+int main() {
+    float a, b, c=0;
+    float quad, rect,tri,trap;
+    cin >> a >> b >> c;
+    tri=(a*b)/2;
+    quad=(a*a);
+    rect=(a*b);
+    trap=((a+b)*c)/2;
+    cout <<  tri<<endl ;
+    cout <<  quad <<endl;
+    cout <<  rect<<endl ;
+    cout <<  trap<< endl;
+    return 0;
 }
